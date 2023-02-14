c975b2e9253d1ac3a0ae7b1c15c7f88e0f1b2c1f
setyawan1234 <twentyzee1@gmail.com>
2023-02-14 22:26:42 +0700

    first commit

---
+++ restructuring.js
@@ -0,0 +1,9 @@
+var pemainSepakbila = {
+    name :'Salah',
+    height : '175',
+    output(){
+        alert('Mr. ' + (this.name) + ' is ' + (this.height) + ' centimeter tail')
+    }
+};
+
+pemainSepakbila.output();
